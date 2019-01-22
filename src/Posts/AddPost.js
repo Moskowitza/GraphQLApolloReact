7bdfcda69e12606ca80bc74985cacef6f0b2bdcb
moskowitza <moskowitzaaron@mac.com>
2019-01-22 12:14:00 -0500

    form working

--- src/Posts/AddPost.js
+++ src/Posts/AddPost.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
-// import PostForm from "./PostForm";
+import PostForm from "./PostForm";
+import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
+
+const NEW_POST = gql`
+  mutation addPost($title: String!, $body: String!) {
+    createPost(data: { status: PUBLISHED, title: $title, body: $body }) {
+      title
+      body
+      id
+    }
+  }
+`;
 
 export default class AddPost extends Component {
   state = {
@@ -17,23 +28,45 @@ export default class AddPost extends Component {
     return (
       <>
         <h1> Add A Post</h1>
-        <form>
-          <input
-            name="title"
-            type="text"
-            value={title}
-            onChange={this.handleInput}
-            placeholder="New Title"
-          />
-          <textarea
-            name="body"
-            type="text"
-            value={body}
-            onChange={this.handleInput}
-            placeholder="Body..."
-          />
-          <button>Submit</button>
-        </form>
+        <Mutation
+          mutation={NEW_POST}
+          variables={{
+            title,
+            body,
+          }}
+        >
+          {createPost => (
+            <form
+              onSubmit={e => {
+                e.preventDefault();
+                createPost()
+                  .then(() => {
+                    this.setState({
+                      title: "",
+                      body: "",
+                    });
+                  })
+                  .catch(err => console.log(err));
+              }}
+            >
+              <input
+                name="title"
+                type="text"
+                value={title}
+                onChange={this.handleInput}
+                placeholder="New Title"
+              />
+              <textarea
+                name="body"
+                type="text"
+                value={body}
+                onChange={this.handleInput}
+                placeholder="Body..."
+              />
+              <button>Submit</button>
+            </form>
+          )}
+        </Mutation>
       </>
     );
   }
